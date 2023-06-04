8ba870f1c04abb5085d89b0e58dc079a38249c49
Santiago Ramirez <94815926+SantiiRepair@users.noreply.github.com>
2023-06-04 12:54:34 -0400

    Create data_analysis.R

---
+++ scripts/data_analysis.R
@@ -0,0 +1,17 @@
+library(httr)
+
+library(jsonlite)
+
+url <- "https://disease.sh/v3/covid-19/historical/Venezuela?lastdays=all"
+
+response <- GET(url)
+
+data <- fromJSON(rawToChar(response$content))
+
+data_df <- data.frame(date = names(data$timeline$cases),
+
+                      cases = unlist(data$timeline$cases),
+
+                      deaths = unlist(data$timeline$deaths))
+
+write.csv(data_df, "data/covid19_data_api.csv", row.names = FALSE)
