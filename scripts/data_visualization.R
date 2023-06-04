1cfec1f9d3e288cae0ba53ade583e62c6e333dbf
Santiago Ramirez <94815926+SantiiRepair@users.noreply.github.com>
2023-06-04 12:56:57 -0400

    Create data_visualization.R

---
+++ scripts/data_visualization.R
@@ -0,0 +1,23 @@
+library(ggplot2)
+
+data_clean <- read.csv("data/covid19_data_clean.csv")
+
+ggplot(data_clean, aes(x = date, y = cases)) +
+
+  geom_bar(stat = "identity") +
+
+  labs(title = "COVID-19 Cases in Venezuela",
+
+       x = "Date",
+
+       y = "Number of cases")
+
+ggplot(data_clean, aes(x = date, y = deaths)) +
+
+  geom_line() +
+
+  labs(title = "COVID-19 Deaths in Venezuela",
+
+       x = "Date",
+
+       y = "Number of deaths")
